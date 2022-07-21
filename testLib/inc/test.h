5f1b35f4a398b88fae76ae7f8f850e433d4d51af
Christoph Kolhoff <93308857+chk1990@users.noreply.github.com>
2022-07-21 13:25:37 +0200

    Added more files

---
+++ testLib/inc/test.h
@@ -0,0 +1,11 @@
+/**
+ * @file test.h
+ * @author Christoph Kolhoff
+ */
+
+#ifndef _CK_TEST_HEADER_
+#define _CK_TEST_HEADER_
+
+void printName(void);
+
+#endif // _CK_TEST_HEADER_
---
+++ testLib/src/test.cpp
@@ -0,0 +1,12 @@
+/**
+ * @file test.h
+ * @author Christoph Kolhoff
+ */
+
+#include "test.h"
+
+#include <iostream>
+
+void printName(void) {
+    std::cout << "Hello world!" << std::endl;
+}
